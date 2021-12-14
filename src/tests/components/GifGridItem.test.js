8db7bba2e211cb7c508b085fac440368fcf1049b
Fernando Schwalb <fschwalb@gmail.com>
2021-12-14 18:50:07 -0300

    First Test

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { shallow } from "enzyme";
+import { GifGridItem } from "../../components/GifGridItem";
+
+describe('Pruebas en <GifGridItem />', () => {
+
+    test('debe de mostrar el componente correctamente', () => {
+
+        const wrapper = shallow( <GifGridItem /> )
+
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+    
+    
+});
