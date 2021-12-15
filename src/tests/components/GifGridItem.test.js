5031fd05d384cbaf04dc736a6f263684de2a2d9f
Fernando Schwalb <fschwalb@gmail.com>
2021-12-14 21:26:28 -0300

    Third Test

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -6,15 +6,42 @@ import { GifGridItem } from "../../components/GifGridItem";
 describe('Pruebas en <GifGridItem />', () => {
 
     const title = 'Un título';
-    const url = 'https://localhost/algo.jpg'
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow( <GifGridItem title={ title } url={ url } /> );
 
     test('debe de mostrar el componente correctamente', () => {
-
-        const wrapper = shallow( <GifGridItem title={ title } url={ url } /> )
 
         expect( wrapper ).toMatchSnapshot();
 
     });
+
+    test('debe de tener un párrafo con el title', () => {
+        
+        const p = wrapper.find( 'p' );
+        expect( p.text().trim() ).toBe( title ) ;
+
+    });
+
+    test('debe de tener una imágen igual al url y alt de los props', () => {
+        
+        const img = wrapper.find( 'img' );
+        // console.log( img.prop('src') );
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+
+    });
+
+    test('debe de tener animate_fadeIn', () => {
+        
+        const div = wrapper.find( 'div' );
+        const className = div.prop( 'className' );
+
+        expect( className.includes('animate_fadeIn') ).toBe( true );
+
+    })
+    
+    
+    
     
     
 });
